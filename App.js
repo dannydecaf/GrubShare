ffacde0259e6c4843fc2868c220f34838ec9355d
Dan Coffey <dcoffey90@gmail.com>
2024-03-13 21:09:58 +0000

    Fixed icon bug and downloaded navigation libraries

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './navigation/appNavigation';
+import AppNavigation from './src/navigation/appNavigation';
 
 export default function App() {
   return (
