b70f6f01e0fcf5162fa55254193dfe5b9636c135
Dan Coffey <dcoffey90@gmail.com>
2024-03-13 22:07:33 +0000

    Added Tailwind CSS compatibility

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,8 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView>
+        <View className="flex-1 bg-neutral-800">
             <Text>HomeScreen</Text>
-        </SafeAreaView>
+        </View>
     )
 }
