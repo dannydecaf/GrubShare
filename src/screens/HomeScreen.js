f17ec752487e8b78e17cbadbb2c3a77cff56db16
Dan Coffey <dcoffey90@gmail.com>
2024-03-13 23:09:05 +0000

    Fixed Tailwind bug, changed homescreen slightly

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import { Bars3CenterLeftIcon } from 'react-native-heroicons/outline';
 
+const ios = Platform.OS === 'ios';
 export default function HomeScreen() {
     return (
         <View className="flex-1 bg-neutral-800">
-            <Text>HomeScreen</Text>
+            {/* search bar and logo */}
+            <SafeAreaView className={ios? "-mb-2": 'mb-3'}>
+                <StatusBar style="light" />
+                <View className="flex-row justify-between items-center mx-4">
+                    <Bars3CenterLeftIcon size="30" strokeWidth={2} color ="white" />
+                    <Text className="text-white text-3xl font-bold">
+                        GrubShare
+                    </Text>
+                </View>
+            </SafeAreaView>
         </View>
     )
 }
