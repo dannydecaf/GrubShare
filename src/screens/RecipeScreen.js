767e750bfd60b99fbeb25dc5a5b9759884c81c70
Dan Coffey <dcoffey90@gmail.com>
2024-03-14 19:07:49 +0000

    Made pic to render on card click & added gradient

--- src/screens/RecipeScreen.js
+++ src/screens/RecipeScreen.js
@@ -1,9 +1,10 @@
-import { View, Text, SafeAreaView, TouchableOpacity, ScrollView, Dimensions, Platform } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity, ScrollView, Dimensions, Platform, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { ChevronLeftIcon } from 'react-native-heroicons/outline';
 import { HeartIcon } from 'react-native-heroicons/solid';
-import { styles } from '../theme';
+import { styles, theme } from '../theme';
+import { LinearGradient } from 'expo-linear-gradient';
 
 var {width, height} = Dimensions.get('window');
 const ios = Platform.OS === 'ios';
@@ -11,6 +12,7 @@ const topMargin = ios? '': 'mt-3';
 
 export default function RecipeScreen() {
     const {params: item} = useRoute();
+    const [isFavourite, toggleFavourite] = useState(false);
     const navigation = useNavigation();
     useEffect(()=>{
         //Call the Receip details API
@@ -27,10 +29,23 @@ export default function RecipeScreen() {
                     <TouchableOpacity onPress={()=> navigation.goBack()} style={styles.background} className="rounded-xl p-1">
                     <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
                     </TouchableOpacity>
-                    <TouchableOpacity>
-                        <HeartIcon size="35" color="white" />
+                    <TouchableOpacity onPress={()=> toggleFavourite(!isFavourite)}>
+                        <HeartIcon size="35" color={isFavourite? theme.background: "white"} />
                     </TouchableOpacity>
                 </SafeAreaView>
+                <View>
+                    <Image
+                    source={require('../assets/carbonara.jpg')}
+                    style={{width, height: height*0.55}}
+                    />
+                    <LinearGradient
+                        colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
+                        style={{width, height: height*0.40}}
+                        start={{x: 0.5, y: 0}}
+                        end={{x: 0.5, y: 1}}
+                        className="absolute bottom-0"
+                        />
+            </View>
             </View>
     </ScrollView>
   )
