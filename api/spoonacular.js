d80e828e4e9dfdc0725694074c09b44407e50e28
Dan Coffey <dcoffey90@gmail.com>
2024-08-25 21:25:29 +0100

    Added pagination to categories within homepage

--- api/spoonacular.js
+++ api/spoonacular.js
@@ -1,63 +1,56 @@
 import axios from 'axios';
 import { apiKey } from '../src/constants';
 
-
-// Endpoints
-
 const apiBaseUrl = 'https://api.spoonacular.com';
 const featuredRecipesEndpoint = `${apiBaseUrl}/recipes/random?number=3&apiKey=${apiKey}`;
-const popularRecipesEndpoint = `${apiBaseUrl}/recipes/complexSearch?sort=popularity&number=10&apiKey=${apiKey}`;
-const recipeDetailsEndpoint = (id) => `${apiBaseUrl}/recipes/${id}/information?includeNutrition=true&apiKey=${apiKey}`;
+const popularRecipesEndpoint = (offset) => `${apiBaseUrl}/recipes/complexSearch?sort=popularity&offset=${offset}&number=10&apiKey=${apiKey}`;
 const similarRecipesEndpoint = (id) => `${apiBaseUrl}/recipes/${id}/similar?apiKey=${apiKey}`;
-const healthyRecipesEndpoint = `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&sort=healthiness&number=10`;
-const budgetFriendlyRecipesEndpoint = (maxPrice) => `${apiBaseUrl}/recipes/complexSearch?maxPricePerServing=${maxPrice}&number=10&apiKey=${apiKey}`;
-const lowCalorieRecipesEndpoint = `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&maxCalories=400&number=10`;
+const healthyRecipesEndpoint = (offset) => `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&sort=healthiness&offset=${offset}&number=10`;
+const budgetFriendlyRecipesEndpoint = (maxPrice, offset) => `${apiBaseUrl}/recipes/complexSearch?maxPricePerServing=${maxPrice}&offset=${offset}&number=10&apiKey=${apiKey}`;
+const lowCalorieRecipesEndpoint = (offset) => `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&maxCalories=400&offset=${offset}&number=10`;
 const ingredientDetailsEndpoint = (id) => `${apiBaseUrl}/food/ingredients/${id}/information?amount=1&apiKey=${apiKey}`;
 const recipesByIngredientEndpoint = (ingredientName) => `${apiBaseUrl}/recipes/findByIngredients?ingredients=${ingredientName}&number=10&apiKey=${apiKey}`;
-const searchRecipesEndpoint = (query) => `${apiBaseUrl}/recipes/complexSearch?query=${query}&number=10&apiKey=${apiKey}`;
+const searchRecipesEndpoint = (query, offset = 0) => `${apiBaseUrl}/recipes/complexSearch?query=${query}&offset=${offset}&number=20&apiKey=${apiKey}`;
 
-
-
-const apiCall = async (endpoint, params)=>{
+const apiCall = async (endpoint, params) => {
     const options = {
         method: 'GET',
         url: endpoint,
-        params: params? params: {}
+        params: params ? params : {}
     };
 
-    try{
+    try {
         const response = await axios.request(options);
         return response.data;
-    }catch(error){
-        console.log('error: ',error);
-        return {}
+    } catch (error) {
+        console.log('error: ', error);
+        return {};
     }
 };
 
-export const fetchFeaturedRecipes = ()=> {
+export const fetchFeaturedRecipes = () => {
     return apiCall(featuredRecipesEndpoint);
 }
 
-export const fetchPopularRecipes = ()=> {
-    return apiCall(popularRecipesEndpoint);
+export const fetchPopularRecipes = (offset = 0) => {
+    return apiCall(popularRecipesEndpoint(offset));
 }
 
-export const fetchHealthyRecipes = () => {
-    return apiCall(healthyRecipesEndpoint);
+export const fetchHealthyRecipes = (offset = 0) => {
+    return apiCall(healthyRecipesEndpoint(offset));
 };
 
-export const fetchBudgetFriendlyRecipes = (maxPrice) => {
-    return apiCall(budgetFriendlyRecipesEndpoint(maxPrice));
+export const fetchBudgetFriendlyRecipes = (maxPrice, offset = 0) => {
+    return apiCall(budgetFriendlyRecipesEndpoint(maxPrice, offset));
 };
 
-export const fetchLowCalorieRecipes = () => {
-    return apiCall(lowCalorieRecipesEndpoint);
+export const fetchLowCalorieRecipes = (offset = 0) => {
+    return apiCall(lowCalorieRecipesEndpoint(offset));
 };
 
 export const fetchRecipeDetails = (id) => {
     return apiCall(recipeDetailsEndpoint(id));
 };
-
 
 export const fetchSimilarRecipes = (id) => {
     return apiCall(similarRecipesEndpoint(id));
@@ -72,6 +65,5 @@ export const fetchRecipesByIngredient = (ingredientName) => {
 };
 
 export const searchRecipes = (query, offset = 0) => {
-    const endpoint = `${apiBaseUrl}/recipes/complexSearch?query=${query}&offset=${offset}&number=20&apiKey=${apiKey}`;
-    return apiCall(endpoint);
+    return apiCall(searchRecipesEndpoint(query, offset));
 }
--- src/screens/LowCalorieRecipesScreen.js
+++ src/screens/LowCalorieRecipesScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -7,15 +7,34 @@ import {
   TouchableOpacity,
   Dimensions,
 } from "react-native";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { fetchLowCalorieRecipes } from "../../api/spoonacular";
 
 const { width } = Dimensions.get("window");
 
 export default function LowCalorieRecipesScreen() {
   const navigation = useNavigation();
-  const route = useRoute();
-  const { data } = route.params;
+  const [recipes, setRecipes] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    loadRecipes();
+  }, []);
+
+  const loadRecipes = async () => {
+    if (!hasMore) return;
+
+    const newRecipes = await fetchLowCalorieRecipes(offset);
+    if (newRecipes.results.length > 0) {
+      setRecipes((prevRecipes) => [...prevRecipes, ...newRecipes.results]);
+      setOffset((prevOffset) => prevOffset + 10);
+      if (newRecipes.results.length > 10) setHasMore(false);
+    } else {
+      setHasMore(false);
+    }
+  };
 
   return (
     <ScrollView style={{ backgroundColor: "#121212" }}>
@@ -25,7 +44,7 @@ export default function LowCalorieRecipesScreen() {
           <TouchableOpacity
             onPress={() => navigation.goBack()}
             style={{
-              backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black background
+              backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent black background
               borderRadius: 10,
               padding: 6,
               flexDirection: "row",
@@ -38,30 +57,36 @@ export default function LowCalorieRecipesScreen() {
         <Text style={{ color: "white", fontSize: 24, marginBottom: 16 }}>
           Under 400 Calories Recipes
         </Text>
-        {data &&
-          data.map((item, index) => (
-            <TouchableOpacity
-              key={index}
-              onPress={() => navigation.navigate("Recipe", { id: item.id })}
-              style={{ marginBottom: 16 }}
-            >
-              <Image
-                source={{
-                  uri: `https://spoonacular.com/recipeImages/${item.id}-556x370.${item.imageType}`,
-                }}
-                style={{
-                  width: width * 0.9,
-                  height: width * 0.5,
-                  borderRadius: 15,
-                }}
-              />
-              <Text style={{ color: "white", fontSize: 18, marginTop: 8 }}>
-                {item.title.length > 30
-                  ? item.title.slice(0, 30) + "..."
-                  : item.title}
-              </Text>
-            </TouchableOpacity>
-          ))}
+        {recipes.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => navigation.navigate("Recipe", { id: item.id })}
+            style={{ marginBottom: 16 }}
+          >
+            <Image
+              source={{
+                uri: `https://spoonacular.com/recipeImages/${item.id}-556x370.${item.imageType}`,
+              }}
+              style={{
+                width: width * 0.9,
+                height: width * 0.5,
+                borderRadius: 15,
+              }}
+            />
+            <Text style={{ color: "white", fontSize: 18, marginTop: 8 }}>
+              {item.title.length > 30
+                ? item.title.slice(0, 30) + "..."
+                : item.title}
+            </Text>
+          </TouchableOpacity>
+        ))}
+        {hasMore && (
+          <TouchableOpacity onPress={loadRecipes}>
+            <Text style={{ color: "yellow", textAlign: "center", margin: 20 }}>
+              Load More
+            </Text>
+          </TouchableOpacity>
+        )}
       </View>
     </ScrollView>
   );
