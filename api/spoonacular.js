08b678d582cf7a5f81bbfb9b4f3a5ad9299c1e7c
Dan Coffey <dcoffey90@gmail.com>
2024-08-25 22:03:45 +0100

    Fixed bug with Featured Recipes

--- api/spoonacular.js
+++ api/spoonacular.js
@@ -3,13 +3,14 @@ import { apiKey } from '../src/constants';
 
 const apiBaseUrl = 'https://api.spoonacular.com';
 const featuredRecipesEndpoint = `${apiBaseUrl}/recipes/random?number=3&apiKey=${apiKey}`;
-const popularRecipesEndpoint = (offset) => `${apiBaseUrl}/recipes/complexSearch?sort=popularity&offset=${offset}&number=10&apiKey=${apiKey}`;
+const popularRecipesEndpoint = (offset = 0) => `${apiBaseUrl}/recipes/complexSearch?sort=popularity&offset=${offset}&number=10&apiKey=${apiKey}`;
+const recipeDetailsEndpoint = (id) => `${apiBaseUrl}/recipes/${id}/information?includeNutrition=true&apiKey=${apiKey}`;
 const similarRecipesEndpoint = (id) => `${apiBaseUrl}/recipes/${id}/similar?apiKey=${apiKey}`;
-const healthyRecipesEndpoint = (offset) => `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&sort=healthiness&offset=${offset}&number=10`;
-const budgetFriendlyRecipesEndpoint = (maxPrice, offset) => `${apiBaseUrl}/recipes/complexSearch?maxPricePerServing=${maxPrice}&offset=${offset}&number=10&apiKey=${apiKey}`;
-const lowCalorieRecipesEndpoint = (offset) => `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&maxCalories=400&offset=${offset}&number=10`;
+const healthyRecipesEndpoint = (offset = 0) => `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&sort=healthiness&offset=${offset}&number=10`;
+const budgetFriendlyRecipesEndpoint = (maxPrice, offset = 0) => `${apiBaseUrl}/recipes/complexSearch?maxPricePerServing=${maxPrice}&offset=${offset}&number=10&apiKey=${apiKey}`;
+const lowCalorieRecipesEndpoint = (offset = 0) => `${apiBaseUrl}/recipes/complexSearch?apiKey=${apiKey}&maxCalories=400&offset=${offset}&number=10`;
 const ingredientDetailsEndpoint = (id) => `${apiBaseUrl}/food/ingredients/${id}/information?amount=1&apiKey=${apiKey}`;
-const recipesByIngredientEndpoint = (ingredientName) => `${apiBaseUrl}/recipes/findByIngredients?ingredients=${ingredientName}&number=10&apiKey=${apiKey}`;
+const recipesByIngredientEndpoint = (ingredientName, offset = 0) => `${apiBaseUrl}/recipes/findByIngredients?ingredients=${ingredientName}&offset=${offset}&number=10&apiKey=${apiKey}`;
 const searchRecipesEndpoint = (query, offset = 0) => `${apiBaseUrl}/recipes/complexSearch?query=${query}&offset=${offset}&number=20&apiKey=${apiKey}`;
 
 const apiCall = async (endpoint, params) => {
@@ -30,21 +31,21 @@ const apiCall = async (endpoint, params) => {
 
 export const fetchFeaturedRecipes = () => {
     return apiCall(featuredRecipesEndpoint);
-}
+};
 
-export const fetchPopularRecipes = (offset = 0) => {
+export const fetchPopularRecipes = (offset) => {
     return apiCall(popularRecipesEndpoint(offset));
-}
+};
 
-export const fetchHealthyRecipes = (offset = 0) => {
+export const fetchHealthyRecipes = (offset) => {
     return apiCall(healthyRecipesEndpoint(offset));
 };
 
-export const fetchBudgetFriendlyRecipes = (maxPrice, offset = 0) => {
+export const fetchBudgetFriendlyRecipes = (maxPrice, offset) => {
     return apiCall(budgetFriendlyRecipesEndpoint(maxPrice, offset));
 };
 
-export const fetchLowCalorieRecipes = (offset = 0) => {
+export const fetchLowCalorieRecipes = (offset) => {
     return apiCall(lowCalorieRecipesEndpoint(offset));
 };
 
@@ -60,10 +61,10 @@ export const fetchIngredientDetails = (id) => {
     return apiCall(ingredientDetailsEndpoint(id));
 };
 
-export const fetchRecipesByIngredient = (ingredientName) => {
-    return apiCall(recipesByIngredientEndpoint(ingredientName));
+export const fetchRecipesByIngredient = (ingredientName, offset) => {
+    return apiCall(recipesByIngredientEndpoint(ingredientName, offset));
 };
 
 export const searchRecipes = (query, offset = 0) => {
     return apiCall(searchRecipesEndpoint(query, offset));
-}
+};
